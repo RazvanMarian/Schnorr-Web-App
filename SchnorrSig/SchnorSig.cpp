e0a0e3ff7734f19ad2d689b9371422c2678f6a3c
razvan <marianrazvan84@yahoo.com>
2021-11-15 17:16:07 +0200

    Aderare semnare confomr BSI-TR-03111

--- SchnorrSig/SchnorSig.cpp
+++ SchnorrSig/SchnorSig.cpp
@@ -22,13 +22,14 @@
 #define ORDER_ERROR 108
 #define SIGNATURE_ERROR 110
 #define KEY_ERROR 112
+#define VERIFICATION_ERROR 114
 
 extern "C"
 {
 
     typedef struct schnorr_signature
     {
-        EC_POINT *R;
+        BIGNUM *R;
         BIGNUM *s;
     };
 
@@ -145,17 +146,14 @@ extern "C"
         return 0;
     }
 
-    int Sign(EC_KEY *key, schnorr_signature &sig, const unsigned char *message, int len)
+    int Schnorr_Sign(EC_KEY *key, const char *message, int message_length, schnorr_signature &sig)
     {
-        const BIGNUM *a;
-        EC_POINT *G;
-        BIGNUM *x, *y, *k, *order, *e;
-        BN_CTX *ctx;
+        EC_POINT *G, *Q;
+        BIGNUM *x, *y, *k, *order, *xQ;
         EC_GROUP *group;
 
         try
         {
-
             group = EC_GROUP_new_by_curve_name(NID_secp256k1);
             if (group == nullptr)
             {
@@ -163,12 +161,7 @@ extern "C"
                 throw GROUP_ERROR;
             }
 
-            sig.R = EC_POINT_new(group);
-            if (sig.R == nullptr)
-            {
-                std::cout << "Memory error" << std::endl;
-                throw MEMORY_ERROR;
-            }
+            //Getting the order of the curve
             order = BN_new();
             if (order == nullptr)
             {
@@ -180,15 +173,17 @@ extern "C"
                 std::cout << "Order error" << std::endl;
                 throw ORDER_ERROR;
             }
+            //*************************************************************************************************
+
+            //Generate random integer k between [1,order]
             k = BN_new();
             if (k == nullptr)
             {
                 std::cout << "Memory error" << std::endl;
                 throw MEMORY_ERROR;
             }
-
-            //Generate random integer k between [1,order]
             BN_rand_range(k, order);
+            //*************************************************************************************************
 
             //BASE POINT G
             G = EC_POINT_new(group);
@@ -212,61 +207,74 @@ extern "C"
                 std::cout << "The point does not belong to the curve!" << std::endl;
                 throw POINT_ERROR;
             }
+            //*************************************************************************************************
 
-            //Calculate R = k*G
-            EC_POINT_mul(group, sig.R, NULL, G, k, NULL);
-
-            if (!EC_POINT_is_on_curve(group, sig.R, NULL))
+            //Calculate Q = k * G
+            Q = EC_POINT_new(group);
+            if (Q == nullptr)
+            {
+                std::cout << "Memory error" << std::endl;
+                throw MEMORY_ERROR;
+            }
+            EC_POINT_mul(group, Q, NULL, G, k, NULL);
+            if (!EC_POINT_is_on_curve(group, Q, NULL))
             {
                 std::cout << "The point does not belong to the curve!" << std::endl;
                 throw POINT_ERROR;
             }
+            //*************************************************************************************************
 
-            unsigned char *r = new unsigned char[65];
-            if (r == nullptr)
+            //Get xQ
+            xQ = BN_new();
+            if (xQ == nullptr)
             {
                 std::cout << "Memory error" << std::endl;
                 throw MEMORY_ERROR;
             }
-            //Conversion from point to buffer for signing
-            int size = EC_POINT_point2buf(group, sig.R, POINT_CONVERSION_HYBRID, &r, nullptr);
+            EC_POINT_get_affine_coordinates(group, Q, xQ, NULL, NULL);
+            printf("\n");
 
-            if (size == 0)
-            {
-                std::cout << "Point conversion error" << std::endl;
-                throw POINT_CONVERSION_ERROR;
-            }
+            unsigned char *xQ_OS = new unsigned char[BN_num_bytes(xQ)];
+            BN_bn2bin(xQ, (unsigned char *)xQ_OS);
+            //*************************************************************************************************
 
-            unsigned char *toHash = new unsigned char[len + size];
-            unsigned char *hash = new unsigned char[32];
-            memcpy(toHash, message, len);
-            memcpy(toHash + len, r, size);
+            // M || xQ
+            unsigned char *temp = new unsigned char[message_length + BN_num_bytes(xQ)];
+            memcpy(temp, message, message_length);
+            memcpy(temp + message_length, xQ_OS, BN_num_bytes(xQ));
+            //*************************************************************************************************
 
-            //Calculate H(M || R)
-            SHA256(toHash, len + size, hash);
+            //Hash ( M || xQ)
+            unsigned char *hash = new unsigned char[SHA256_DIGEST_LENGTH];
+            SHA256(temp, message_length + BN_num_bytes(xQ), hash);
+            delete[](temp);
+            delete[](xQ_OS);
+            //*************************************************************************************************
 
-            e = BN_new();
-            if (e == nullptr)
+            //Calculate r = Hash(M || xQ)
+            sig.R = BN_new();
+            if (sig.R == nullptr)
             {
                 std::cout << "Memory error" << std::endl;
                 throw MEMORY_ERROR;
             }
-            //Calculate e=H(M || R)
-            BN_bin2bn(hash, 32, e);
 
-            ctx = BN_CTX_new();
+            BN_bin2bn(hash, SHA256_DIGEST_LENGTH, sig.R);
+            BN_CTX *ctx = BN_CTX_new();
+            BN_mod(sig.R, sig.R, order, ctx);
+            delete[](hash);
+            //*************************************************************************************************
+
+            //Apoi s = (k - r * private_key) mod n
+            //Output r,s
+            const BIGNUM *private_key = BN_new();
+            private_key = EC_KEY_get0_private_key(key);
+
+            BIGNUM *temporary = BN_new();
             sig.s = BN_new();
-            if (sig.s == nullptr)
-            {
-                std::cout << "Memory error" << std::endl;
-                throw MEMORY_ERROR;
-            }
 
-            //Calculate s=(k+a*e) mod order
-            a = BN_new();
-            a = EC_KEY_get0_private_key(key);
-            BN_mod_mul(e, e, a, order, ctx);
-            BN_mod_add(sig.s, k, e, order, ctx);
+            BN_mod_mul(temporary, sig.R, private_key, order, ctx);
+            BN_mod_sub(sig.s, k, temporary, order, ctx);
         }
         catch (int err)
         {
@@ -275,23 +283,21 @@ extern "C"
         }
 
         EC_POINT_free(G);
+        EC_POINT_free(Q);
         BN_free(order);
-        BN_free(e);
         BN_free(x);
         BN_free(y);
         BN_free(k);
+        BN_free(xQ);
         EC_GROUP_free(group);
-
         return 0;
     }
 
-    int Verify(EC_KEY *key, schnorr_signature &sig, const unsigned char *message, int len)
+    int Verify_Sign(EC_KEY *key, const char *message, int message_length, schnorr_signature &sig)
     {
-        const EC_POINT *Q;
-        EC_POINT *G, *eByQ, *sByG;
-        BIGNUM *e, *x, *y;
+        BIGNUM *order, *x, *y;
+        EC_POINT *G, *Q;
         EC_GROUP *group;
-
         try
         {
             group = EC_GROUP_new_by_curve_name(NID_secp256k1);
@@ -301,85 +307,31 @@ extern "C"
                 throw GROUP_ERROR;
             }
 
-            unsigned char *r = new unsigned char[65];
-            if (r == nullptr)
+            //Getting the order of the curve
+            order = BN_new();
+            if (order == nullptr)
             {
                 std::cout << "Memory error" << std::endl;
                 throw MEMORY_ERROR;
             }
-
-            int size = EC_POINT_point2buf(group, sig.R, POINT_CONVERSION_HYBRID, &r, nullptr);
-            if (size == 0)
+            if (EC_GROUP_get_order(group, order, nullptr) == 0)
             {
-                std::cout << "Point conversion error" << std::endl;
-                throw POINT_CONVERSION_ERROR;
+                std::cout << "Order error" << std::endl;
+                throw ORDER_ERROR;
             }
+            //*************************************************************************************************
 
-            unsigned char *toHash = new unsigned char[len + size];
-            if (toHash == nullptr)
+            //Check if is in the normal bounds
+            BIGNUM *test = BN_new();
+            BN_one(test);
+            if ((BN_cmp(sig.s, order) == 1) || (BN_cmp(sig.s, test) == -1))
             {
-                std::cout << "Memory error" << std::endl;
-                throw MEMORY_ERROR;
+                std::cout << "S component of the signature is out of bounds";
+                throw VERIFICATION_ERROR;
             }
+            BN_free(test);
+            //*************************************************************************************************
 
-            unsigned char *hash = new unsigned char[32];
-            if (hash == nullptr)
-            {
-                std::cout << "Memory error" << std::endl;
-                throw MEMORY_ERROR;
-            }
-            memcpy(toHash, message, len);
-            memcpy(toHash + len, r, size);
-
-            //Calculate H(M || R)
-            SHA256(toHash, len + size, hash);
-
-            e = BN_new();
-            if (e == nullptr)
-            {
-                std::cout << "Memory error" << std::endl;
-                throw MEMORY_ERROR;
-            }
-            //Calculate e=H(M || R)
-            BN_bin2bn(hash, 32, e);
-
-            eByQ = EC_POINT_new(group);
-            if (eByQ == nullptr)
-            {
-                std::cout << "Memory error" << std::endl;
-                throw MEMORY_ERROR;
-            }
-
-            sByG = EC_POINT_new(group);
-            if (sByG == nullptr)
-            {
-                std::cout << "Memory error" << std::endl;
-                throw MEMORY_ERROR;
-            }
-
-            //Verification
-            //R + e*Q = s*G
-            Q = EC_POINT_new(group);
-            if (Q == nullptr)
-            {
-                std::cout << "Memory error" << std::endl;
-                throw MEMORY_ERROR;
-            }
-            Q = EC_KEY_get0_public_key(key);
-            //e*Q
-            if (!EC_POINT_mul(group, eByQ, NULL, Q, e, NULL))
-            {
-                std::cout << "Calculation error" << std::endl;
-                throw CALCULATION_ERROR;
-            }
-            //R+e*Q
-            if (!EC_POINT_add(group, eByQ, sig.R, eByQ, NULL))
-            {
-                std::cout << "Calculation error" << std::endl;
-                throw CALCULATION_ERROR;
-            }
-
-            //s*G
             //BASE POINT G
             G = EC_POINT_new(group);
             if (G == nullptr)
@@ -402,30 +354,92 @@ extern "C"
                 std::cout << "The point does not belong to the curve!" << std::endl;
                 throw POINT_ERROR;
             }
+            //*************************************************************************************************
 
-            if (!EC_POINT_mul(group, sByG, NULL, G, sig.s, NULL))
+            //Q = s * G
+            Q = EC_POINT_new(group);
+            //s * G
+            EC_POINT_mul(group, Q, NULL, G, sig.s, NULL);
+            const EC_POINT *P = EC_KEY_get0_public_key(key);
+            EC_POINT *T = EC_POINT_new(group);
+
+            //r * P
+            EC_POINT_mul(group, T, NULL, P, sig.R, NULL);
+
+            //s * G + r * P
+            EC_POINT_add(group, Q, Q, T, NULL);
+
+            if (EC_POINT_is_at_infinity(group, Q))
             {
-                std::cout << "Calculation error" << std::endl;
-                throw CALCULATION_ERROR;
+                std::cout << "Verification error" << std::endl;
+                throw VERIFICATION_ERROR;
             }
 
-            //Final comparison
-            int end = EC_POINT_cmp(group, eByQ, sByG, NULL);
-            if (end != 0)
+            //get xQ
+            BIGNUM *xQ = BN_new();
+            if (xQ == nullptr)
             {
-                std::cout << "The signature is not ok!" << std::endl;
-                return SIGNATURE_ERROR;
+                std::cout << "Memory error" << std::endl;
+                throw MEMORY_ERROR;
             }
+            EC_POINT_get_affine_coordinates(group, Q, xQ, NULL, NULL);
+            printf("\n");
+
+            unsigned char *xQ_OS = new unsigned char[BN_num_bytes(xQ)];
+            BN_bn2bin(xQ, (unsigned char *)xQ_OS);
+            //*************************************************************************************************
+
+            // M || xQ
+            unsigned char *temp = new unsigned char[message_length + BN_num_bytes(xQ)];
+            memcpy(temp, message, message_length);
+            memcpy(temp + message_length, xQ_OS, BN_num_bytes(xQ));
+            //*************************************************************************************************
+
+            //Hash ( M || xQ)
+            unsigned char *hash = new unsigned char[SHA256_DIGEST_LENGTH];
+            SHA256(temp, message_length + BN_num_bytes(xQ), hash);
+            //*************************************************************************************************
+
+            //Calculate v = Hash(M || xQ)
+            BIGNUM *v = BN_new();
+            if (v == nullptr)
+            {
+                std::cout << "Memory error" << std::endl;
+                throw MEMORY_ERROR;
+            }
+
+            BN_bin2bn(hash, SHA256_DIGEST_LENGTH, v);
+            delete[](hash);
+            delete[](xQ_OS);
+            BN_free(xQ);
+
+            BN_CTX *ctx = BN_CTX_new();
+            BN_mod(v, v, order, ctx);
+            BN_CTX_free(ctx);
+            //*************************************************************************************************
+
+            //Compare v with r
+            //if v == r => verification successful
+            if (BN_cmp(v, sig.R) == 0)
+            {
+                std::cout << "VERIFICATION OK" << std::endl;
+            }
+            else
+            {
+                std::cout << "Verification error" << std::endl;
+                throw VERIFICATION_ERROR;
+            }
+            BN_free(v);
         }
         catch (int err)
         {
             std::cout << "ERROR: " << err << std::endl;
             return err;
         }
-        EC_POINT_free(eByQ);
-        EC_POINT_free(sByG);
+
         EC_POINT_free(G);
-        BN_free(e);
+        EC_POINT_free(Q);
+        BN_free(order);
         BN_free(x);
         BN_free(y);
         EC_GROUP_free(group);
@@ -547,7 +561,7 @@ extern "C"
         }
 
         schnorr_signature sig;
-        res = Sign(key, sig, (unsigned char *)hash, SHA256_DIGEST_LENGTH);
+        res = Schnorr_Sign(key, hash, SHA256_DIGEST_LENGTH, sig);
         if (res != 0)
         {
             std::cout << "Eroare la generarea crearea semnaturii" << std::endl;
@@ -555,13 +569,13 @@ extern "C"
         }
         BN_print_fp(stdout, sig.s);
         std::cout << std::endl;
-        res = Verify(key, sig, (unsigned char *)hash, SHA256_DIGEST_LENGTH);
+        res = Verify_Sign(key, hash, SHA256_DIGEST_LENGTH, sig);
         if (res != 0)
         {
             std::cout << "Eroare la verificare semnatura";
             return;
         }
 
-        std::cout << "Generare, Semnare, Verificare OK" << std::endl;
+        std::cout << "Generare, Semnare, Verificare OK :)" << std::endl;
     }
 }
